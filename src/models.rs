f18449fbdea10ccc9a0d90dfe975d5e17349d4a1
Nuttakit Kundum <nuttakitkundum@gmail.com>
2025-05-18 17:03:20 +0700

    feat: refactor ProveRequest struct and remove VerifyRequest and TransactionMessage

--- src/models.rs
+++ src/models.rs
@@ -1,24 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
-pub struct VerifyRequest {
-    pub proof: String,
-    pub public_inputs: Vec<String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 pub struct ProveRequest {
-    pub secret: String,
     pub otp_code: u32,
-    pub message: TransactionMessage,
+    pub secret_b32: String,
+    pub tx_msg: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct TransactionMessage {
-    pub to: String,
-    pub value: String,
-    pub data: String,
-    pub nonce: u64,
-    pub chain_id: u64,
-    pub action: String,
+#[derive(Debug, Serialize)]
+pub struct ProveSuccessResponse {
+    pub valid: bool,
+    pub tx_nouce: u32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct ProveErrorResponse {
+    pub valid: bool,
+    pub reason: String,
 }
