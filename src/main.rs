5772061a14bdf75358d1f4ee75aae70e6ecd7153
Nuttakit Kundum <nuttakitkundum@gmail.com>
2025-05-18 22:56:32 +0700

    refactor: simplify tracing subscriber initialization in main function

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,15 @@ mod types;
 
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, App, HttpServer};
-use tracing_subscriber::EnvFilter;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::from_default_env().add_directive("actix_web=info".parse().unwrap()),
-        )
-        .init();
+    tracing_subscriber::fmt().init();
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
-            .wrap(Cors::permissive()) // tighten later
+            .wrap(Cors::permissive())
             .configure(routes::register)
     })
     .bind(("0.0.0.0", 8080))?
